bf5415d8ad5620a9b45c4a0bfbcf8b2dd0e3248d
vian <alfian.jw@gmail.com>
2023-09-15 21:28:41 +0700

    update 1.0

---
+++ randomizer_test.go
@@ -0,0 +1,32 @@
+package basic
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRandomizer(t *testing.T) {
+	randomizer := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+	fmt.Println(randomizer.Intn(42))
+	fmt.Println(randomizer.Uint32())
+	fmt.Println(randomizer.Float32())
+ 
+}
+
+func TestStringRandomizer(t *testing.T) {
+	stringRandomizer := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+	letter := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	var amba = func (length int) string {
+		b := make([]rune, length)
+		for i := range b {
+			b[i] = letter[stringRandomizer.Intn(len(letter))]
+		}
+		return string(b)
+	}
+
+	fmt.Println("Random String 5 karakter", amba(4))
+	
+}
